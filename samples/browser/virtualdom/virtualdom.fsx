771d5ed77ba38943fe404bd3917f646b1f305d98
Tomas Jansson <tomas.jansson@gmail.com>
2016-06-12 21:03:16 +0200

    stuff working

--- samples/browser/virtualdom/virtualdom.fsx
+++ samples/browser/virtualdom/virtualdom.fsx
@@ -13,19 +13,110 @@
 (*** hide ***)
 #r "node_modules/fable-core/Fable.Core.dll"
 open Fable.Core
-open Fable.Import.Browser
+//open Fable.Import.Browser
 
+type private QueueCell<'T> =
+  { Value : 'T
+    mutable Next : option<QueueCell<'T>> }
+
+/// Simple queue implemented as linked list with `Add`
+/// to add things to the end & `TryGet` to remove first item
+type private Queue<'T>() =
+    let mutable firstAndLast = None
+
+    member x.Add(it:'T) =
+        let itCell = { Value = it; Next = None }
+        match firstAndLast with
+        | None -> firstAndLast <- Some(itCell, itCell)
+        | Some(first, last) ->
+            last.Next <- Some itCell
+            firstAndLast <- Some(first, itCell)
+
+    member x.TryGet() =
+        match firstAndLast with
+        | None -> None
+        | Some({ Value = value; Next = None}, _) ->
+            firstAndLast <- None
+            Some value
+        | Some({ Value = value; Next = Some next}, last) ->
+            firstAndLast <- Some(next, last)
+            Some value
+
+/// Represents a reply channel used by the `PostAndAsyncReply` method of
+/// `MailboxProcessor<'T>` (same idea as standard `AsyncReplyChannel<'T>`)
+type ReplyChannel<'T>(f:'T -> unit) =
+    member x.Reply(res) = f res
+
+/// A lightweight implementation of F# `MailboxProcessor`. As the standard one,
+/// you can start it using `MailboxProcessor.Start(fun inbox -> ...)` and you
+/// then call `inbox.Receive` inside the body and `agent.Post` anywhere.
+type MailboxProcessor<'T>(body) =
+    let mutable messages = Queue<'T>()
+    let mutable continuation = None
+    let processEvents () =
+        continuation |> Option.iter (fun cont ->
+            let fm = messages.TryGet()
+            fm |> Option.iter (fun value ->
+              continuation <- None
+              cont value ) )
+
+    member x.Start() =
+        body x |> Async.StartImmediate
+
+    static member Start(body) =
+        let mbox = MailboxProcessor<_>(body)
+        mbox.Start()
+        mbox
+
+    member x.Receive() = Async.FromContinuations(fun c ->
+        if Option.isSome continuation then failwith "Receive can only be called once!"
+        continuation <- Some (unbox c) // Here be dragons
+        processEvents () )
+
+    member x.PostAndAsyncReply<'R>(f) : Async<'R> =
+        let result = ref None
+        let continuation = ref None
+        let checkCompletion () =
+            match result.Value, continuation.Value with
+            | Some res, Some cont -> cont res
+            | _ -> ()
+        let reply =
+            ReplyChannel<'R>(fun res ->
+                result.Value <- Some res
+                checkCompletion () )
+        messages.Add(f reply)
+        processEvents ()
+        Async.FromContinuations(fun (c, _, _) ->
+            continuation.Value <- Some c
+            checkCompletion ())
+
+    member x.Post(msg) =
+        messages.Add(msg)
+        processEvents ()
 
 module Html =
 //    type Attribute = string * string
 
-    type Handler = unit -> unit
+    type MouseEvent =
+        {
+            altKey: bool
+            screenX: int
+            screenY: int
+        }
+
+    type MouseEventHandler =
+        | OnClick of (MouseEvent -> unit)
+
+    type EventHandler =
+        | MouseEventHandler of MouseEventHandler
+
     type Style =
         { border : string }
+
     type KeyValue = string*string
 
     type Attribute =
-    | Handler of string*Handler
+    | EventHandler of EventHandler
     | Style of Style
     | KeyValue of KeyValue
 
@@ -42,6 +133,11 @@ module Html =
     /// Whitespace for formatting
     | WhiteSpace of string
 
+    let mouseClick f =
+        EventHandler (MouseEventHandler (OnClick f))
+
+
+open Fable.Import.Browser
 let failwithjs() = failwith "JS only"
 let [<Import("default","virtual-dom/h")>] h(arg1: string, arg2: obj, arg3: obj[]): obj = failwithjs()
 let [<Import("default","virtual-dom/diff")>] diff(arg1:obj, arg2:obj): obj = failwithjs()
@@ -51,15 +147,25 @@ let [<Import("default","virtual-dom/create-element")>] createElement:obj -> Node
 [<Emit("String($0)")>]
 let String i :obj= failwith "JS only"
 
+[<Emit("x + y")>]
+let something x y = failwith "JS only"
+
 module VDom =
     open Html
 
     let rec render node =
+        printfn "%A" node
+
+        let renderMouseEventHandler = function
+            | OnClick h -> "onclick", (h :> obj)
+
+        let renderHandler = function
+            | MouseEventHandler mh -> renderMouseEventHandler mh
 
         let toAttrs attrs =
             attrs
             |> List.map (function
-                    | Handler (evt,handler) -> evt, handler :> obj
+                    | EventHandler handler -> handler |> renderHandler
                     | Style style -> "style", (style :> obj)
                     | KeyValue (key, value) -> key,(value :> obj)
                 )
@@ -77,7 +183,34 @@ module VDom =
 
     let div attrs children = Element(("div", attrs), children)
 
-type AppState<'TModel, 'TMessage> = { Model: 'TModel; View: 'TModel -> ('TMessage -> unit) -> Html.Node; Update: 'TMessage -> 'TModel -> 'TModel}
+type AppState<'TModel, 'TMessage> = {
+        Model: 'TModel;
+        View: 'TModel -> ('TMessage -> unit) -> Html.Node;
+        Update: 'TMessage -> 'TModel -> 'TModel}
+
+type Observer<'T>(next, error, completed) =
+    interface System.IObserver<'T> with
+        member x.OnCompleted() = completed()
+        member x.OnError(e) = error e
+        member x.OnNext(v) = next v
+
+type Microsoft.FSharp.Control.Async with
+  /// Returns an asynchronous workflow that waits for the first
+  /// occurrence of the specified event & then unsubscribes
+  static member AwaitObservable (obs:System.IObservable<'T>) =
+    Async.FromContinuations(fun (cont, econt, ccont) ->
+        let disp : ref<option<System.IDisposable>> = ref None
+        let onNext v =
+          disp.Value.Value.Dispose()
+          cont(v)
+        let onError e =
+          disp.Value.Value.Dispose()
+          econt(e)
+        let onComplete () =
+          disp.Value.Value.Dispose()
+          ccont (unbox (System.Exception("Observable stream ended.")))
+        disp := obs.Subscribe(Observer(onNext, onError, onComplete)) |> Some)
+
 
 type App<'TModel, 'TMessage> = {AppState: AppState<'TModel, 'TMessage>; Node: Node option; CurrentTree: obj option}
 
@@ -110,29 +243,57 @@ let start app =
             }
         loop {AppState = app; Node = None; CurrentTree = None})
 
+
+//    let inbox = new Event<Action>()
+//
+//    let rec loop state =
+//        async {
+//            match state.Node, state.CurrentTree with
+//            | None,_ ->
+//                let tree = createTree state.AppState.View state.AppState.Model inbox.Trigger
+//                let rootNode = createElement tree
+//                document.body.appendChild(rootNode) |> ignore
+//                return! loop {state with CurrentTree = Some tree; Node = Some rootNode}
+//
+//            | Some rootNode, Some currentTree ->
+//                let! message = inbox.Publish |> Async.AwaitObservable
+//                let model' = state.AppState.Update message state.AppState.Model
+//                let tree = createTree state.AppState.View model' inbox.Trigger
+//                let patches = diff(currentTree, tree)
+//                patch(rootNode, patches) |> ignore
+//                return! loop {state with AppState = {state.AppState with Model = model'}; CurrentTree = Some tree}
+//            | _ -> failwith "Shouldn't happen"
+//        }
+//    loop {AppState = app; Node = None; CurrentTree = None}
+
+open VDom
 let view m handler =
-    VDom.div
+    div
         [
             Html.Style {border = "1px solid red"}
-            Html.Handler ("onclick",(fun() -> handler Increment))
         ]
-        [Html.Text (string m)]
+        [
+            div [Html.Style { border = "1px solid blue"}; Html.mouseClick (fun x -> window.alert((x.screenX, x.screenY):> obj); handler Increment)] [Html.Text (string "Increment")]
+            Html.Text (string m)
+            div [Html.Style { border = "1px solid green"}; Html.mouseClick (fun x -> window.alert((x.screenX, x.screenY):> obj); handler Decrement)] [Html.Text (string "Decrement")]
+        ]
 
 let update msg model =
     match msg with
     | Increment -> model + 1
     | Decrement -> model - 1
 
-let hello (model) =
-    VDom.div
-        [
-            Html.Style {border = "1px solid red"}
-            Html.Handler ("onclick",(fun() -> window.alert("Clicked") |> ignore))
-        ]
-        [Html.Text (string model)]
-    |> VDom.render
-
 start {Model = 0; View = view; Update = update}
+
+//let hello (model) =
+//    VDom.div
+//        [
+//            Html.Style {border = "1px solid red"}
+//            Html.Handler ("onclick",(fun() -> window.alert("Clicked") |> ignore))
+//        ]
+//        [Html.Text (string model)]
+//    |> VDom.render
+
 //  h("div", createObj [ "style" ==> { border = "1px solid red" } ], [| String count |])
 
 //let mutable tree = hello 45
