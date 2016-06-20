9c42c3308d4e137a3c2c23cb0095aabf6451bdda
Tomas Jansson <tomas.jansson@gmail.com>
2016-06-20 09:48:04 +0200

    starting clean up

--- samples/browser/virtualdom/virtualdom.fsx
+++ samples/browser/virtualdom/virtualdom.fsx
@@ -1,16 +1,3 @@
-(**
- - title: Super Fable Mario
- - tagline: Mario clone using HTML5 canvas
- - app-style: height:384px; width:512px; margin:20px auto 20px auto; position:relative;
- - intro: Mario clone, based on functional reactive [sample written in
-   Elm](http://debug.elm-lang.org/edit/Mario.elm). The Fable version is using HTML 5
-   canvas to render the background and an `img` tag showing the Mario (using animated GIFs).
-   You can find the [full source code on GitHub](https://github.com/fsprojects/Fable/blob/master/samples/browser/mario/mario.fsx).
-
-   To see how it works, use the left, right and up buttons to play. Our Mario respects
-   no boundaries!
-*)
-(*** hide ***)
 #load "html.fsx"
 #load "App.fsx"
 #r "node_modules/fable-core/Fable.Core.dll"
@@ -20,6 +7,7 @@ open Fable.Import.Browser
 open App
 open Html
 
+// Todo model
 type Filter =
     | All
     | Completed
@@ -38,18 +26,6 @@ type TodoModel =
         Filter: Filter
     }
 
-type Model =
-    {
-        Counter: int
-        Todo: TodoModel
-    }
-
-type CounterAction =
-    | Increment
-    | IncrementWith of int
-    | Decrement
-    | DecrementWith of int
-
 type TodoAction =
     | AddItem of Item
     | ChangeInput of string
@@ -60,33 +36,8 @@ type TodoAction =
     | UnCheckAll
     | SetActiveFilter of Filter
     | ClearCompleted
-type Action =
-    | CounterAction of CounterAction
-    | TodoAction of TodoAction
 
-
-    //        <section class="todoapp">
-    //        			<header class="header">
-    //        				<h1>todos</h1>
-    //        				<input class="new-todo" placeholder="What needs to be done?" autofocus="">
-    //        			</header>
-    //        			<footer class="footer" style="display: block;">
-    //        				<span class="todo-count"><strong>2</strong> items left</span>
-    //        				<ul class="filters">
-    //        					<li>
-    //        						<a href="#/" class="selected">All</a>
-    //        					</li>
-    //        					<li>
-    //        						<a href="#/active">Active</a>
-    //        					</li>
-    //        					<li>
-    //        						<a href="#/completed">Completed</a>
-    //        					</li>
-    //        				</ul>
-    //        				<button class="clear-completed" style="display: none;"></button>
-    //        			</footer>
-    //        		</section>
-
+// Todo view
 let filterToTextAndUrl = function
     | All -> "All", ""
     | Completed -> "Completed", "completed"
@@ -127,10 +78,6 @@ let todoFooter model handler =
 
 
 let todoHeader model handler =
-//        			<header class="header">
-//        				<h1>todos</h1>
-//        				<input class="new-todo" placeholder="What needs to be done?" autofocus="">
-//        			</header>
     header
         [attribute "class" "header"]
         [   h1 [] [text "todos"]
@@ -145,7 +92,6 @@ let todoHeader model handler =
                     onKeyup (fun x -> handler (ChangeInput (x?target?value :?> string))) ]]
 
 let listItem handler item =
-// <li data-id="1466286291319" class=""><div class="view"><input class="toggle" type="checkbox"><label>erwerwe</label><button class="destroy"></button></div></li>
     let itemChecked = if item.Done then "true" else ""
     li [ attribute "class" (if item.Done then "completed" else "")]
        [ div [ attribute "class" "view" ]
@@ -172,7 +118,7 @@ let todoMain model handler =
     let items = model.Items
     let allChecked = items |> List.exists (fun i -> not i.Done)
     section [  attribute "class" "main"
-               property "style" "display: block;" ]
+               Style [ "style", "block" ] ]
             [   input [ property "id" "toggle-all"
                         attribute "class" "toggle-all"
                         property "type" "checkbox"
@@ -188,40 +134,24 @@ let todoMain model handler =
 let todoView m handler =
     section
         [attribute "class" "todoapp"]
-        ((todoHeader m.Input handler)::(if m.Items = []
+        ((todoHeader m.Input handler)::(if m.Items |> List.isEmpty
                 then []
                 else [  (todoMain m handler)
                         (todoFooter m handler) ] ))
 
-let view m handler =
-    div
-        [
-            Style ["border","1px solid red"]
-        ]
-        [
-            div [   Style ["border","1px solid blue"]
-                    onMouseClick (fun x -> handler (CounterAction Increment))
-                    onDblClick (fun x -> handler (CounterAction (IncrementWith 100)))] [text (string "Increment")]
-            text (string m.Counter)
-            div [   Style ["border", "1px solid green"; "height", ((string (70+m.Counter)) + "px")]
-                    onMouseClick (fun x -> handler (CounterAction Decrement))
-                    onDblClick (fun x -> handler (CounterAction (DecrementWith 50)))]
-                [text (string "Decrement")]
-            todoView m.Todo (fun x -> handler (Action.TodoAction x))
-        ]
-
-let updateTodo msg model =
+// Todo update
+let todoUpdate msg model =
     let checkAllWith v =
         { model with Items = model.Items |> List.map (fun i -> { i with Done = v })}
 
     match msg with
     | AddItem item ->
         let maxId =
-            if model.Items = [] then 1
+            if model.Items |> List.isEmpty then 1
             else
                 model.Items
                 |> List.map (fun x -> x.Id)
-                |> List.maxBy (fun x -> x)
+                |> List.max
         let item' = {item with Id = maxId + 1}
         {model with Items = item'::model.Items; Input = ""}
     | ChangeInput v -> {model with Input = v}
@@ -239,21 +169,62 @@ let updateTodo msg model =
     | SetActiveFilter f -> { model with Filter = f }
     | ClearCompleted -> { model with Items = model.Items |> List.filter (fun i -> not i.Done)}
 
+start {Model = {Filter = All; Items = [ ]; Input = ""}; View = todoView; Update = todoUpdate}
 
-let counterUpdate msg model =
-    match msg with
-    | Increment -> model + 1
-    | IncrementWith x -> model + x
-    | Decrement -> model - 1
-    | DecrementWith x -> model - 1
-
-let update msg model =
-    let model' =
-        printfn "Handling: %A" msg
-        match msg with
-        | CounterAction a -> {model with Counter = (counterUpdate a model.Counter)}
-        | TodoAction a -> {model with Todo = (updateTodo a model.Todo)}
-    printfn "New model %A" model'
-    model'
-
-start {Model = {Counter = 0; Todo = {Filter = All; Items = [ { Name = "Yolo"; Done = false; Id = 0}]; Input = ""}}; View = view; Update = update}
+//type Model =
+//    {
+//        Counter: int
+//        Todo: TodoModel
+//    }
+//
+//type CounterAction =
+//    | Increment
+//    | IncrementWith of int
+//    | Decrement
+//    | DecrementWith of int
+//
+//type Action =
+//    | CounterAction of CounterAction
+//    | TodoAction of TodoAction
+//
+//let counterView m handler =
+//    div []
+//        [
+//            div [   Style ["border","1px solid blue"]
+//                    onMouseClick (fun x -> handler (Increment))
+//                    onDblClick (fun x -> handler ((IncrementWith 100)))] [text (string "Increment")]
+//            text (string m)
+//            div [   Style ["border", "1px solid green"; "height", ((string (70+m)) + "px")]
+//                    onMouseClick (fun x -> handler (Decrement))
+//                    onDblClick (fun x -> handler (DecrementWith 50))]
+//                [text (string "Decrement")]
+//        ]
+//
+//
+//let view m handler =
+//    div
+//        [
+//            Style ["border","1px solid red"]
+//        ]
+//        [
+//            counterView m.Counter (Action.CounterAction >> handler)
+//            todoView m.Todo (Action.TodoAction >> handler)
+//        ]
+//
+//let counterUpdate msg model =
+//    match msg with
+//    | Increment -> model + 1
+//    | IncrementWith x -> model + x
+//    | Decrement -> model - 1
+//    | DecrementWith x -> model - 1
+//
+//let update msg model =
+//    let model' =
+//        printfn "Handling: %A" msg
+//        match msg with
+//        | CounterAction a -> {model with Counter = (counterUpdate a model.Counter)}
+//        | TodoAction a -> {model with Todo = (todoUpdate a model.Todo)}
+//    printfn "New model %A" model'
+//    model'
+//
+//start {Model = {Counter = 0; Todo = {Filter = All; Items = [ { Name = "Yolo"; Done = false; Id = 0}]; Input = ""}}; View = view; Update = update}
